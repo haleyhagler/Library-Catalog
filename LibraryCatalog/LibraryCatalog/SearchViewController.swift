cb0176410e45fac38029e850d088c3df61fe5630
haleyhagler <haleyhagler@gmail.com>
2019-03-29 22:09:41 -0500

    Project Started
    The Xcode project was created. Pod files where added

---
+++ LibraryCatalog/LibraryCatalog/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  LibraryCatalog
+//
+//  Created by Haley Hagler on 3/29/19.
+//  Copyright © 2019 Haley Hagler. All rights reserved.
+//
+
+import Cocoa
+
+class SearchViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ LibraryCatalog/LibraryCatalog/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LibraryCatalog
+//
+//  Created by Haley Hagler on 3/29/19.
+//  Copyright © 2019 Haley Hagler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
